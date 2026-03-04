5a7d0c3ce5870f7f940126e8d8d5bcdd3ccb9811
Jayson Roda <jayson.roda@awsys-i.com>
2026-03-04 16:27:35 +0800

    reinstall prisma

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Req } from '@nestjs/common';
+import type { Request } from 'express';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+  @Get('login')
+  login(@Req() request: Request): string {
+    return 'test'
+  }
+}
